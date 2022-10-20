030170ff7a805faed470aaa1f424084c784fe182
PakkoFrank48 <pakko.pakk@gmail.com>
2022-10-20 02:50:22 -0500

    que traidor

---
+++ Eje_1/main.js
@@ -0,0 +1,26 @@
+let Edad = document.querySelector("#edad");
+document.querySelector("#btn").addEventListener("click", ()=>{
+
+    let año = 19;
+
+    if(año > 18 ){
+
+        console.log("Eres mayor de edad");
+    
+        ///alert("Eres mayor de edad");
+    
+    
+    
+    }else{
+        //alert("eres menor de edad")
+        console.log("Eres menor de edad");
+    
+    }
+    
+})
+
+
+
+
+
+
